0455915a6df4c2ad113c4b0cf2781aaa72d7a136
Bertrand Gouny <bertrand.gouny@osixia.net>
2017-08-22 16:50:15 +0200

    update packages

--- image/service/mmc-web/install.sh
+++ image/service/mmc-web/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 # this script is run during the image build
 
-dpkg -i /container/service/mmc-web/assets/package/mmc-web-base_3.9.92-1_all.deb
-dpkg -i /container/service/mmc-web/assets/package/mmc-web-dashboard_3.9.92-1_all.deb
+dpkg -i --ignore-depends=libapache2-mod-php5,php5-gd,php5-xmlrpc /container/service/mmc-web/assets/package/mmc-web-base_3.9.90-10_all.deb
+dpkg -i /container/service/mmc-web/assets/package/mmc-web-dashboard_3.9.90-10_all.deb
 dpkg -i /container/service/mmc-web/assets/package/mmc-web-mail_2.5.1-1_all.deb
 dpkg -i /container/service/mmc-web/assets/package/mmc-web-sshlpk_2.5.1-1_all.deb
 
